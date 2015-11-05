cdabab7b8a46dc1c80636f3573fa19895f97b984
blublud <pdpham@ucdavis.edu>
2015-11-04 18:48:35 -0700

    Updated setup_git.sh

--- setup_git.sh
+++ setup_git.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/tcsh
 
 #Load credentials into vars
-script_dir=$(dirname $0)
-bash $script_dir/setup_git.sh.private
+set script_dir=`dirname $0`
 
+source $script_dir/setup_git.sh.private
+
+echo "set git username to: $GIT_USRNAME"
 git config --global user.name $GIT_USRNAME
+echo "set git email to: $GIT_EMAIL"
 git config --global user.email $GIT_EMAIL
 git config --global color.ui auto
 
-eval `ssh-agent -s`
+eval `ssh-agent -c`
 ssh-add $GIT_PRIVATEKEY
